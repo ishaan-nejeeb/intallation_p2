89c470eacc335433b3a2fb085a54f27dd930e0be
ishaan-nejeeb <nejeeb.ishaan@gmail.com>
2022-12-11 14:13:03 +0530

    alpha version ready. need to add data. add sound. add extra features

--- sketch.js
+++ sketch.js
@@ -8,6 +8,10 @@ let l1Init, l1End, l2Init, l2End, l3Init, l3End, l4Init, l4End, l5Init, l5End;
 let pMill = 0;
 let toBeShown;
 let randNum;
+let started = false;
+let alpha = 255;
+let fade = 0;
+let fader = 3;
 
 function setup() {
   createCanvas(1280, 720);
@@ -23,7 +27,7 @@ function setup() {
   //     l5Init,
   //     (l5End = createVector(0, 0));
   console.log(data[0].events[5]);
-  count = 5;
+  count = 0;
   randNum = int(random(data.lenght));
 }
 
@@ -31,11 +35,19 @@ function draw() {
   //background(220);
   console.log(count);
   let mill = millis();
-  console.log(data[randNum].events[count]);
-  if (data[randNum].events[count]) {
-    toBeShown = data[0].initText;
+  //console.log(data[randNum].events[count]);
+  if (!started) {
+    toBeShown = data[randNum].initText;
     writeText();
-  } else if (count % 5 == 0 && mill - pMill > 2000) {
+    if (alpha <= 0) {
+      alpha = 255;
+      started = true;
+    }
+  } else if (data[randNum].events[count]) {
+    toBeShown = data[randNum].events[count];
+    console.log(toBeShown);
+    writeText();
+  } else if (count > 0 && count % 5 == 0 && mill - pMill > 2000) {
     background(255);
     pMill = mill;
   } else if (mouseIsPressed) {
@@ -61,10 +73,10 @@ function mouseReleased() {
 
 function checkLine() {
   if (count % 5 != 0) {
-    //count++;
+    count++;
     //addToStore();
   } else {
-    //count++;
+    count++;
     //addToStore();
     pMill = millis();
     console.log(pMill);
@@ -73,16 +85,18 @@ function checkLine() {
 
 function writeText() {
   background(255);
-  fill(0);
+  fill(0, alpha);
   textFont("Cormorant Garamond");
   textAlign(CENTER);
   textSize(24);
   text(toBeShown, width / 2 - 450, height / 2, 900);
+  alpha -= fade;
 }
 
 function keyPressed() {
   if (keyCode === 66) {
     console.log("Key  typed");
+    fade = fader;
   }
 }
 
