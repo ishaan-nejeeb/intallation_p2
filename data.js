5ea7b109d7b78438c1814cbded5189bf9eb5f9f9
unknown <nejeeb.ishaan@gmail.com>
2022-12-11 13:25:44 +0530

    First commit

---
+++ data.js
@@ -0,0 +1,10 @@
+data = [
+  {
+    name: "ABC",
+    dateIncarcerated: "21-10-2021",
+    initText: "intittextinintextinintext",
+    events: {
+      5: "Hello",
+    },
+  },
+];
---
+++ sketch.js
@@ -0,0 +1,110 @@
+let initMouseX,
+  initMouseY = 0;
+let endMouseX,
+  endMouseY = 0;
+let count,
+  pCount = 0;
+let l1Init, l1End, l2Init, l2End, l3Init, l3End, l4Init, l4End, l5Init, l5End;
+let pMill = 0;
+let toBeShown;
+let randNum;
+
+function setup() {
+  createCanvas(1280, 720);
+  background(255);
+  //   l1Init,
+  //     l1End,
+  //     l2Init,
+  //     l2End,
+  //     l3Init,
+  //     l3End,
+  //     l4Init,
+  //     l4End,
+  //     l5Init,
+  //     (l5End = createVector(0, 0));
+  console.log(data[0].events[5]);
+  count = 5;
+  randNum = int(random(data.lenght));
+}
+
+function draw() {
+  //background(220);
+  console.log(count);
+  let mill = millis();
+  console.log(data[randNum].events[count]);
+  if (data[randNum].events[count]) {
+    toBeShown = data[0].initText;
+    writeText();
+  } else if (count % 5 == 0 && mill - pMill > 2000) {
+    background(255);
+    pMill = mill;
+  } else if (mouseIsPressed) {
+    strokeWeight(4);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+  //console.log(mouseX,mouseY);
+}
+
+function mousePressed() {
+  initMouseX = mouseX;
+  initMouseY = mouseY;
+  //console.log("hello",initMouseX, initMouseY);
+}
+
+function mouseReleased() {
+  endMouseX = pmouseX;
+  endMouseY = pmouseY;
+  checkLine();
+  //console.log("hello",endMouseX, endMouseY);
+  //addToStore();
+}
+
+function checkLine() {
+  if (count % 5 != 0) {
+    //count++;
+    //addToStore();
+  } else {
+    //count++;
+    //addToStore();
+    pMill = millis();
+    console.log(pMill);
+  }
+}
+
+function writeText() {
+  background(255);
+  fill(0);
+  textFont("Cormorant Garamond");
+  textAlign(CENTER);
+  textSize(24);
+  text(toBeShown, width / 2 - 450, height / 2, 900);
+}
+
+function keyPressed() {
+  if (keyCode === 66) {
+    console.log("Key  typed");
+  }
+}
+
+// function addToStore(){
+//   if (count % 5 == 1){
+//     l1Init.set(initMouseX, initMouseY);
+//     l1End.set(initMouseX, endMouseX);
+//   }
+//   else if (count % 5 == 2){
+//     l2Init.set(initMouseX, initMouseY);
+//     l2End.set(initMouseX, endMouseX);
+//   }
+//   else if (count % 5 == 3){
+//     l3Init.set(initMouseX, initMouseY);
+//     l3End.set(initMouseX, endMouseX);
+//   }
+//   else if (count % 5 == 4){
+//     l4Init.set(initMouseX, initMouseY);
+//     l4End.set(initMouseX, endMouseX);
+//   }
+//   else {
+//     l5Init.set(initMouseX, initMouseY);
+//     l5End.set(initMouseX, endMouseX);
+//   }
+// }
